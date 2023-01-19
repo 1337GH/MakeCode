214e35b2e15e93825083e82233c18c336f867e12
1337GH <119616372+1337GH@users.noreply.github.com>
2023-01-19 11:26:12 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,51 @@
-let mySprite = sprites.create(img`
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    while (!(controller.B.isPressed())) {
+    	
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    while (!(controller.B.isPressed())) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 4 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 0, -100)
+        projectile.setFlag(SpriteFlag.AutoDestroy, true)
+        pause(200)
+    }
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
-    . . . . 3 . . . . . . . . . . . 
-    . . . . 3 . . . . . . . . . . . 
-    . . . . . 3 . . . . . . . . . . 
-    . . . . . . 3 . . . . . . . . . 
-    . . . . . . . 3 3 . . . . . . . 
-    . . . . . . . . . 3 3 . . . . . 
-    . . . . . . . . . . . 3 3 . . . 
-    . . . . . . . . . . . . . 3 3 . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . . 7 7 7 . . . . . . . 
+    . . . . . 7 8 7 8 7 . . . . . . 
+    . . . . . 7 7 7 7 7 . . . . . . 
+    . . . . 7 7 8 8 8 7 7 . . . . . 
+    . . . 7 8 8 8 7 8 8 8 7 . . . . 
+    . . . 7 7 7 8 7 8 7 7 7 . . . . 
+    . . . . . 7 8 7 8 7 . . . . . . 
+    . . . . . 7 7 7 7 7 7 . . . . . 
+    . . . . 7 8 7 . 7 8 7 . . . . . 
+    . . . . . 7 . . . 7 . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-mySprite.setVelocity(50, 50)
+mySprite.y = 110
+controller.moveSprite(mySprite, 75, 0)
